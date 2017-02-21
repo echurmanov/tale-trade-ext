6de70503707b068b1aa98a2bc5f3aa897e6c929f
Eugene Churmanov <echurmanov@gmail.com>
2017-02-21 13:15:21 +0500

    исправлена работа обхеденения карт

--- game.js
+++ game.js
@@ -55,6 +55,108 @@ function replaceCardsBlock(){
 
     var combineProcess = false;
 
+
+    function rebuildChoseList() {
+      var n = 0;
+      cardChoicesBlock.find('li[data-original-title]').remove();
+      for (var rarity = 0; rarity <= 4; rarity++) {
+        for (var cardName in processedCards) {
+          if (processedCards.hasOwnProperty(cardName)) {
+            var card = processedCards[cardName];
+            if (card.rarity == rarity) {
+              var captionParts = [];
+              if (card.notAuctionNumber > 0) {
+                var li = $(document.createElement('li'));
+                li.attr('data-original-title','');
+                li.addClass(n%2==0?'odd':'even');
+                li.append("<a href=\"javascript:void(0);\" data-card-auction=\"false\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[rarity]+"\" data-card-name=\""+cardName+"\" style=\"font-size:10pt\">"+cardName+" * ("+card.notAuctionNumber+")</a>");
+                cardChoicesBlock.append(li);
+                n++;
+              }
+              if (card.auctionNumber > 0) {
+                var li = $(document.createElement('li'));
+                li.attr('data-original-title','');
+                li.addClass(n%2==0?'odd':'even');
+                li.append("<a href=\"javascript:void(0);\" data-card-auction=\"true\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[rarity]+"\" data-card-name=\""+cardName+"\" style=\"font-size:10pt\">"+cardName+" ("+card.auctionNumber+")</a>");
+                cardChoicesBlock.append(li);
+                n++;
+              }
+            }
+          }
+        }
+      }
+
+      cardChoicesBlock.find('a').on('click', function(e){
+        if (combineProcess) {
+          return;
+        }
+        var cardLink = $(e.target);
+        var push = false;
+        var clear = false;
+        var cardName = cardLink.attr('data-card-name');
+        var cardAuc = cardLink.attr('data-card-auction') == "true";
+
+        if (cardLink.hasClass("ext-all-used-cards") || cardLink.hasClass("ext-missing-card")) {
+          return;
+        }
+
+        if (typeof processedCards[cardName] == 'undefined') {
+          cardLink.addClass('ext-missing-card');
+          return;
+        }
+
+        if (selectedCards.length == 0 || processedCards[selectedCards[0]].rarity != processedCards[cardName].rarity) {
+          combineBtn.button('more');
+          push = true;
+          selectedCards = [];
+          selectedCardsAuc = [];
+          selectedCards.push(cardName);
+          selectedCardsAuc.push(cardAuc);
+        } else if (selectedCards.length < 3) {
+          if (selectedCards.length < 2 || processedCards[selectedCards[0]].rarity != 4) {
+            selectedCards.push(cardName);
+            selectedCardsAuc.push(cardAuc);
+            push = true;
+          }
+          combineBtn.button('reset');
+        }
+        if (selectedCards.length == 1) {
+          clear = true;
+        }
+        if (clear) {
+          cardSelectedBlock.find("li[data-original-title]").remove();
+        }
+        if (push) {
+          var li = $(document.createElement('li'));
+          li.addClass(n%2==0?'odd':'even');
+          li.attr('data-original-title','');
+
+          li.append("<a href=\"javascript:void(0);\" data-card-auction=\""+cardAuc+"\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[processedCards[cardName].rarity]+"\" style=\"font-size:10pt\">"+cardName+ (cardAuc?"":" *") +"</a>");
+          cardSelectedBlock.append(li);
+          li.find("a").on('click', function(evt){
+            if (combineProcess) {
+              return;
+            }
+            var $el = $(evt.target);
+            var cardName = $el.attr('data-card-name');
+            var cardAuction = $el.attr('data-card-auction') == "true";
+            for (var i = 0; i < selectedCards.length; i++) {
+              if (selectedCards[i] == cardName && selectedCardsAuc[i] == cardAuction) {
+                selectedCards.splice(i, 1);
+                selectedCardsAuc.splice(i, 1);
+                break;
+              }
+            }
+            $el.remove();
+            updateChoseList(selectedCards, selectedCardsAuc);
+          });
+
+          updateChoseList(selectedCards, selectedCardsAuc);
+
+        }
+      });
+    }
+
     combineBtn.on('click', function(){
       if (combineProcess) {
         return;
@@ -62,26 +164,39 @@ function replaceCardsBlock(){
       if (selectedCards.length > 1) {
         combineProcess = true;
         var cardUids = [];
-        var i = 0;
+        var needCards = selectedCards.length;
         for (var cardName in processedCards) {
-          if (cardName == selectedCards[i]) {
-            if (selectedCardsAuc[i]) {
-              cardUids.push(processedCards[cardName].auctionUids.pop());
-              processedCards[cardName].auctionNumber--;
-              i++;
-            } else {
-              cardUids.push(processedCards[cardName].notAuctionUids.pop());
-              processedCards[cardName].notAuctionNumber--;
-              i++;
+          do {
+            var i = selectedCards.indexOf(cardName);
+            if (i != -1) {
+              if (selectedCardsAuc[i]) {
+                cardUids.push(processedCards[cardName].auctionUids.pop());
+                processedCards[cardName].auctionNumber--;
+              } else {
+                cardUids.push(processedCards[cardName].notAuctionUids.pop());
+                processedCards[cardName].notAuctionNumber--;
+              }
+              selectedCards.splice(i, 1);
+              selectedCardsAuc.splice(i, 1);
             }
-          }
-          if (i == selectedCards.length) {
+          } while (i != -1);
+          if (selectedCards.length == 0) {
             break;
           }
         }
+        if (cardUids.length != needCards) {
+          combineProcess = false;
+          spanResultCombine.html("Ошибка: Выбранные карты не были найдены в пуле карт. Пересоберите комбинацию.");
+          selectedCards = [];
+          selectedCardsAuc = [];
+          cardSelectedBlock.find("li[data-original-title]").remove();
+          rebuildChoseList();
+          updateChoseList(selectedCards, selectedCardsAuc);
+          return;
+        }
+
         selectedCards = [];
         selectedCardsAuc = [];
-        console.log(1);
 
         function checkStatus(url) {
           $.ajax({
@@ -89,17 +204,41 @@ function replaceCardsBlock(){
             type: "get",
             success: function (res) {
               if (res.status == "ok") {
+                if (res.data.card_ui_info) {
+                  var rawCards = res.data.card_ui_info;
+                  if (typeof processedCards[rawCards.name] == 'undefined') {
+                    processedCards[rawCards.name] = {
+                      auctionNumber: 0,
+                      notAuctionNumber: 0,
+                      name: rawCards.name,
+                      rarity: rawCards.rarity,
+                      type: rawCards.type,
+                      auctionUids: [],
+                      notAuctionUids: []
+                    }
+                  }
+                  if (rawCards.auction) {
+                    processedCards[rawCards.name].auctionNumber++;
+                    processedCards[rawCards.name].auctionUids.push(rawCards.uid);
+                  } else {
+                    processedCards[rawCards.name].notAuctionNumber++;
+                    processedCards[rawCards.name].notAuctionUids.push(rawCards.uid);
+                  }
+                }
                 spanResultCombine.html(res.data.message);
                 combineProcess = false;
                 combineBtn.button('select');
                 cardSelectedBlock.find("li[data-original-title]").remove();
+                rebuildChoseList();
                 updateChoseList(selectedCards, selectedCardsAuc);
               } else if (res.status == 'processing') {
                 setTimeout(function () {
                   checkStatus(res.status_url);
                 }, 700);
               } else if (res.status == 'error') {
-
+                combineProcess = false;
+                rebuildChoseList();
+                spanResultCombine.html("<strong>Ошибка:</strong> " + res.error);
               }
             }
           });
@@ -115,11 +254,15 @@ function replaceCardsBlock(){
               setTimeout(function(){
                 checkStatus(res.status_url);
               }, 300);
+            } else {
+              combineProcess = false;
+              rebuildChoseList();
+              updateChoseList(selectedCards, selectedCardsAuc);
+              spanResultCombine.html("<strong>Ошибка:</strong> " + res.error);
             }
 
           },
           error: function(res) {
-            console.log("Error",res);
             combineProcess = false;
             cardSelectedBlock.find("li[data-original-title]").remove();
             updateChoseList(selectedCards, selectedCardsAuc);
@@ -128,34 +271,9 @@ function replaceCardsBlock(){
       }
     });
 
+    rebuildChoseList();
     var n = 0;
 
-    for (var rarity = 0; rarity <= 4; rarity++) {
-      for (var cardName in processedCards) {
-        if (processedCards.hasOwnProperty(cardName)) {
-          var card = processedCards[cardName];
-          if (card.rarity == rarity) {
-            var captionParts = [];
-            if (card.notAuctionNumber > 0) {
-              var li = $(document.createElement('li'));
-              li.attr('data-original-title','');
-              li.addClass(n%2==0?'odd':'even');
-              li.append("<a href=\"javascript:void(0);\" data-card-auction=\"false\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[rarity]+"\" data-card-name=\""+cardName+"\" style=\"font-size:10pt\">"+cardName+" * ("+card.notAuctionNumber+")</a>");
-              cardChoicesBlock.append(li);
-              n++;
-            }
-            if (card.auctionNumber > 0) {
-              var li = $(document.createElement('li'));
-              li.attr('data-original-title','');
-              li.addClass(n%2==0?'odd':'even');
-              li.append("<a href=\"javascript:void(0);\" data-card-auction=\"true\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[rarity]+"\" data-card-name=\""+cardName+"\" style=\"font-size:10pt\">"+cardName+" ("+card.auctionNumber+")</a>");
-              cardChoicesBlock.append(li);
-              n++;
-            }
-          }
-        }
-      }
-    }
 
     function updateChoseList(selectedCards, selectedCardsAuc)
     {
@@ -207,69 +325,6 @@ function replaceCardsBlock(){
       }
     }
 
-    cardChoicesBlock.find('a').on('click', function(e){
-      var cardLink = $(e.target);
-      var push = false;
-      var clear = false;
-      var cardName = cardLink.attr('data-card-name');
-      var cardAuc = cardLink.attr('data-card-auction') == "true";
-
-      if (cardLink.hasClass("ext-all-used-cards") || cardLink.hasClass("ext-missing-card")) {
-        return;
-      }
-
-      if (typeof processedCards[cardName] == 'undefined') {
-        cardLink.addClass('ext-missing-card');
-        return;
-      }
-
-      if (selectedCards.length == 0 || processedCards[selectedCards[0]].rarity != processedCards[cardName].rarity) {
-        combineBtn.button('more');
-        push = true;
-        selectedCards = [];
-        selectedCardsAuc = [];
-        selectedCards.push(cardName);
-        selectedCardsAuc.push(cardAuc);
-      } else if (selectedCards.length < 3) {
-        if (selectedCards.length < 2 || processedCards[selectedCards[0]].rarity != 4) {
-          selectedCards.push(cardName);
-          selectedCardsAuc.push(cardAuc);
-          push = true;
-        }
-        combineBtn.button('reset');
-      }
-      if (selectedCards.length == 1) {
-        clear = true;
-      }
-      if (clear) {
-        cardSelectedBlock.find("li[data-original-title]").remove();
-      }
-      if (push) {
-        var li = $(document.createElement('li'));
-        li.addClass(n%2==0?'odd':'even');
-        li.attr('data-original-title','');
-
-        li.append("<a href=\"javascript:void(0);\" data-card-auction=\""+cardAuc+"\" data-card-name=\""+cardName+"\" class=\""+rarityLabelMap[processedCards[cardName].rarity]+"\" style=\"font-size:10pt\">"+cardName+ (cardAuc?"":" *") +"</a>");
-        cardSelectedBlock.append(li);
-        li.find("a").on('click', function(evt){
-          var $el = $(evt.target);
-          var cardName = $el.attr('data-card-name');
-          var cardAuction = $el.attr('data-card-auction') == "true";
-          for (var i = 0; i < selectedCards.length; i++) {
-            if (selectedCards[i] == cardName && selectedCardsAuc[i] == cardAuction) {
-              selectedCards.splice(i, 1);
-              selectedCardsAuc.splice(i, 1);
-              break;
-            }
-          }
-          $el.remove();
-          updateChoseList(selectedCards, selectedCardsAuc);
-        });
-
-        updateChoseList(selectedCards, selectedCardsAuc);
-
-      }
-    });
   };
 
   function modifiedShowCards(container, filterUIDs) {
